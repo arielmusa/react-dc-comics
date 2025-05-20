98b526c17052e9bc8804dba0a49b2897721f28da
arielmusa <arielmusainfo@gmail.com>
2025-05-20 17:19:53 +0200

    create Footer component and add it to App.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,85 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-top">
+          <div className="footer-menu">
+            <div className="footer-links">
+              <h3>DC COMICS</h3>
+              <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+              </ul>
+            </div>
+            <div className="footer-links">
+              <h3>SHOP</h3>
+              <ul>
+                <li>Shop DC</li>
+                <li>Shop DC Collections</li>
+              </ul>
+            </div>
+            <div className="footer-links">
+              <h3>DC</h3>
+              <ul>
+                <li>Terms of use</li>
+                <li>Privacy Policy (new)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+            <div className="footer-links">
+              <h3>SITES</h3>
+              <ul>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+              </ul>
+            </div>
+          </div>
+          <div className="empty-div"></div>
+        </div>
+        <div className="follow-up-banner">
+          <div>
+            <a className="sign-up btn btn-secondary" href="">
+              SIGN-UP NOW!
+            </a>
+          </div>
+          <div>
+            <h3>FOLLOW US</h3>
+            <ul id="social-media-links">
+              <li>
+                <img src="" alt="" />
+              </li>
+              <li>
+                <img src="" alt="" />
+              </li>
+              <li>
+                <img src="" alt="" />
+              </li>
+              <li>
+                <img src="" alt="" />
+              </li>
+              <li>
+                <img src="" alt="" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
