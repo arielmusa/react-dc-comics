59e91303b1b04583ccac076cd7ee57a55f39dd40
arielmusa <arielmusainfo@gmail.com>
2025-05-20 16:07:59 +0200

    create Header component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./assets/css/App.css";
+import Header from "./components/Header";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import logo from "../assets/img/dc-logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div>
+          <img src={logo} alt="DC Logo" />
+        </div>
+        <nav>
+          <ul id="header-menu">
+            <li id="menu-item">Characters</li>
+            <li className="active" id="menu-item">
+              Comics
+            </li>
+            <li id="menu-item">Movies</li>
+            <li id="menu-item">TV</li>
+            <li id="menu-item">Games</li>
+            <li id="menu-item">Collectibles</li>
+            <li id="menu-item">Videos</li>
+            <li id="menu-item">Fans</li>
+            <li id="menu-item">News</li>
+            <li id="menu-item">Shop</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
