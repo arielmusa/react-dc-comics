d4173325273272b07e6a265936efa035d654815a
arielmusa <arielmusainfo@gmail.com>
2025-05-20 16:17:51 +0200

    create Main component and add it to App.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,37 @@
+import digitalComics from "../assets/img/buy-comics-digital-comics.png";
+import dcMerch from "../assets/img/buy-comics-merchandise.png";
+import subscription from "../assets/img/buy-comics-subscriptions.png";
+import comicShop from "../assets/img/buy-comics-shop-locator.png";
+import dcPowerVisa from "../assets/img/buy-dc-power-visa.svg";
+
+export default function Main() {
+  return (
+    <main>
+      <div className="container">
+        <div>Content goes here</div>
+        <div className="icons-banner">
+          <div className="icon-items">
+            <img src={digitalComics} alt="Digital Comics" />
+            Digital Comics
+          </div>
+          <div className="icon-items">
+            <img src={dcMerch} alt="DC Merchandise" />
+            DC Merchandise
+          </div>
+          <div className="icon-items">
+            <img src={subscription} alt="" />
+            Subscription
+          </div>
+          <div className="icon-items">
+            <img src={comicShop} alt="" />
+            Comic Shop Locator
+          </div>
+          <div className="icon-items">
+            <img src={dcPowerVisa} alt="" />
+            DC Power Visa
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
